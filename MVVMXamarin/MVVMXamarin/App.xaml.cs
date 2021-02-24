9c8c71b18e5fbc98e3b6c465819c89f87c532af1
DemetrioQ <52639107+DemetrioQ@users.noreply.github.com>
2021-02-24 17:11:41 -0400

    First commit

---
+++ MVVMXamarin/MVVMXamarin/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var login = new LoginPage();
+            NavigationPage.SetHasNavigationBar(login, false);
+            MainPage = new NavigationPage(login);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MVVMXamarin/MVVMXamarin/Views/LoginPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVMXamarin
+{
+    public partial class LoginPage : ContentPage
+    {
+        private bool canClick = true;
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(email.Text))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error","El campo de email no puede de estar vacio", "Ok");
+            }
+            else if (string.IsNullOrEmpty(password.Text))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "El campo de contraseña no puede de estar vacio", "Ok");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Bienvenido", "Hola, " + email.Text, "Ok");
+                var homePage = new HomeTabbedPage();
+                NavigationPage.SetHasNavigationBar(homePage, false);
+                await Navigation.PushAsync(homePage);
+            }
+            
+        }
+
+        private async void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+
+           await Navigation.PushAsync(new MVVMXamarin.RegisterPage());
+           
+
+        }
+    }
+}
